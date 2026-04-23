848a9c9348f5db4c43b8571ebf8d5327e6fe2def
Daniel Ribeiro <djbreves@gmail.com>
2026-04-23 16:46:16 +1000

    Truncate storage account name to fit 24-char limit

--- infra/app/locals.tf
+++ infra/app/locals.tf
@@ -1,7 +1,7 @@
 locals {
   name          = "${var.app_name}-${var.environment}"
   environment_short = var.environment == "development" ? "dev" : var.environment
-  name_short    = "${replace(var.app_name, "-", "")}${local.environment_short}"
+  name_short    = substr("${replace(var.app_name, "-", "")}${local.environment_short}", 0, 22)
   is_production     = var.environment == "production"
   custom_domain     = local.is_production ? "${var.subdomain}.${var.custom_domain}" : "${var.subdomain}.${local.environment_short}.${var.custom_domain}"
   dns_record_name   = local.is_production ? var.subdomain : "${var.subdomain}.${local.environment_short}"
